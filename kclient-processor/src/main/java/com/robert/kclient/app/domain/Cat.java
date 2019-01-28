4d2270a9e994fe013ad8cb68153e722f3d851e30
kevinten10 <596823919@qq.com>
2019-01-28 14:07:51 +0800

    init

--- kclient-processor/src/main/java/com/robert/kclient/app/domain/Cat.java
+++ kclient-processor/src/main/java/com/robert/kclient/app/domain/Cat.java
@@ -2,48 +2,47 @@ package com.robert.kclient.app.domain;
 
 /**
  * Sample bean for conversion between JSON and object.
- * 
+ *
  * @author Robert Lee
  * @since Aug 21, 2015
- *
  */
 
 public class Cat {
-	private long id;
+    private long id;
 
-	private String name;
+    private String name;
 
-	public Cat() {
+    public Cat() {
 
-	}
+    }
 
-	public Cat(Dog dog) {
-		this.id = dog.getId();
-		this.name = dog.getName();
-	}
+    public Cat(Dog dog) {
+        this.id = dog.getId();
+        this.name = dog.getName();
+    }
 
-	public long getId() {
-		return id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	@Override
-	public String toString() {
-		StringBuffer sb = new StringBuffer("Cat[");
-		sb.append("id=").append(id).append(",");
-		sb.append("name=").append(name).append("]");
-		return sb.toString();
-	}
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer("Cat[");
+        sb.append("id=").append(id).append(",");
+        sb.append("name=").append(name).append("]");
+        return sb.toString();
+    }
 
 };
