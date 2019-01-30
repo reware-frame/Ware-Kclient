bb550f423daf2dcc9e1f9cbd5c57432b0d66fc24
kevinten10 <596823919@qq.com>
2019-01-30 09:31:57 +0800

    read kclient exception

--- kclient-core/src/main/java/com/ten/ware/kafka/kclient/excephandler/DefaultExceptionHandler.java
+++ kclient-core/src/main/java/com/ten/ware/kafka/kclient/excephandler/DefaultExceptionHandler.java
@@ -7,31 +7,30 @@ import org.slf4j.LoggerFactory;
 /**
  * Default exception handler to log the error context in the error log which can
  * be used to retry the message latter manully or by support tool.
- * 
- * @author Robert Lee
- * @since Aug 21, 2015
- *
+ * <p>
+ * 默认异常处理器
  */
 public class DefaultExceptionHandler implements ExceptionHandler {
 
-	// Record the error log for data recovery or wash data by the logs
+    // Record the error log for data recovery or wash data by the logs
 
-	protected static Logger errorRunLog = LoggerFactory.getLogger("error.run."
-			+ SafelyMessageHandler.class);
-	protected static Logger errorShutdownlog = LoggerFactory
-			.getLogger("error.shutdown." + SafelyMessageHandler.class);
+    protected static Logger errorRunLog = LoggerFactory.getLogger("error.run." + SafelyMessageHandler.class);
+    protected static Logger errorShutdownlog = LoggerFactory.getLogger("error.shutdown." + SafelyMessageHandler.class);
 
-	public boolean support(Throwable t) {
-		return true;
-	}
+    @Override
+    public boolean support(Throwable t) {
+        return true;
+    }
 
-	public void handle(Throwable t, String message) {
-		if (t instanceof InterruptedException)
-			errorRunLog.error(
-					"Maybe it is shutting down. Or interruped when handing the message:\t"
-							+ message, t);
-		else
-			errorShutdownlog.error(
-					"Failed to handle the message: \t" + message, t);
-	}
+    @Override
+    public void handle(Throwable t, String message) {
+        if (t instanceof InterruptedException) {
+            errorRunLog.error(
+                    "Maybe it is shutting down. Or interruped when handing the message:\t"
+                            + message, t);
+        } else {
+            errorShutdownlog.error(
+                    "Failed to handle the message: \t" + message, t);
+        }
+    }
 }
--- kclient-core/src/main/java/com/ten/ware/kafka/kclient/excephandler/ExceptionHandler.java
+++ kclient-core/src/main/java/com/ten/ware/kafka/kclient/excephandler/ExceptionHandler.java
@@ -3,13 +3,11 @@ package com.ten.ware.kafka.kclient.excephandler;
 /**
  * This is the pluggable interface to handle the unexpected exception when
  * handing the message in handlers.
- * 
- * @author Robert Lee
- * @since Aug 21, 2015
- *
+ * <p>
+ * 处理异常
  */
 public interface ExceptionHandler {
-	public boolean support(Throwable t);
+    boolean support(Throwable t);
 
-	public void handle(Throwable t, String message);
+    void handle(Throwable t, String message);
 }
